1f70b6080784188b22506c8e8bd64232ad7acfce
Renukachavala476 <chavala.renuka@gmail.com>
2021-07-25 13:19:16 +0530

    all list operations

---
+++ listoperations.py
@@ -0,0 +1,18 @@
+names=["dharani","bharathi","renka","sridhar",101,202,303,401]
+#print(names)
+#print(names[1:3])
+# print(names.pop(2))
+# print(names)
+# del names[0]
+# print(names)
+#print(names*2)
+# region=["kz","nlr","bza","ogl"]
+# #print(names+region)
+# #print(cmp(names,region))
+# if(region in names):
+#     print("exists")
+# else:
+#     print("not exists")    
+#print(tuple(names))
+#print(list(names))
+print(clear(names))
